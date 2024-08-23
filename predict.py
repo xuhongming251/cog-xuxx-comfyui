f529f3984e8284301caea019cd93e60cc24b9a3b
xuminglong <494462498@qq.com>
2024-08-23 18:41:28 +0800

    add comfyui

--- predict.py
+++ predict.py
@@ -1,13 +1,47 @@
-from cog import BasePredictor, Path, Input
- 
+# An example of how to convert a given API workflow into its own Replicate model
+# Replace predict.py with this file when building your own workflow
+
+import os
+import mimetypes
+import json
+import shutil
+from typing import List
+from cog import BasePredictor, Input, Path
+from comfyui import ComfyUI
+from cog_model_helpers import optimise_images
+from cog_model_helpers import seed as seed_helper
+
+OUTPUT_DIR = "/tmp/outputs"
+INPUT_DIR = "/tmp/inputs"
+COMFYUI_TEMP_OUTPUT_DIR = "ComfyUI/temp"
+ALL_DIRECTORIES = [OUTPUT_DIR, INPUT_DIR, COMFYUI_TEMP_OUTPUT_DIR]
+
+mimetypes.add_type("image/webp", ".webp")
+
+# Force HF offline
+os.environ["HF_DATASETS_OFFLINE"] = "1"
+os.environ["TRANSFORMERS_OFFLINE"] = "1"
+os.environ["HF_HUB_DISABLE_TELEMETRY"] = "1"
+
+
 class Predictor(BasePredictor):
     def setup(self):
-        """Load the model into memory to make running multiple predictions efficient"""
-        print("in setup") 
-    def predict(self,
-            image: Path = Input(description="Image"),
-            scale: float = Input(description="Factor", default=1.5)
+        self.comfyUI = ComfyUI("127.0.0.1:8188")
+        self.comfyUI.start_server(OUTPUT_DIR, INPUT_DIR)
+
+    def predict(
+        self,
+        workflow: str = Input(
+            default="",
+        )
     ) -> Path:
-        output = f"hello, {scale}"
-        print(output)
+        
+        print(workflow)
+        
+        self.comfyUI.cleanup(ALL_DIRECTORIES)
+        seed = seed_helper.generate(seed)
+
+        self.comfyUI.connect()
+        self.comfyUI.run_workflow(workflow)
+
         return Path("./test.png")
